b5a30457780edc8df3d9375d2fc9639f63fae872
lucasmetron <lucasmetron@gmail.com>
2021-02-18 15:56:53 -0300

    estudando window e document

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-function criarAluno(nome,n1,n2){
-    this.nome = nome;
-    this.nota1 = n1;
-    this.nota2 = n2;
-    this.media = function (){
-        return (this.nota1 + this.nota2) /2
-    }
-}
- 
- 
-var aluno1 = new criarAluno ('lucas', 8,9);
-var aluno2 = new criarAluno ('pedro', 5,6);
-var aluno3 = new criarAluno ('joao', 10,4);
-var aluno4 = new criarAluno ("bianca", 5, 8)
- 
-console.log(aluno4.nome)
-console.log(aluno4.media())
+document.body.innerHTML = "estou add um txt pelo JS" + document.body.innerHTML
