7dc78d18c0a45c6ceb5434be5228bc44c49c9608
lucasmetron <lucasmetron@gmail.com>
2021-02-22 15:21:31 -0300

    inclusao de eventos direto pelo JS

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-/*function h1click(){
-    console.log("executando a função")
+/*
+
+
+function pronto() {
+    console.log("carregado")
+    let t = document.getElementById("titulo")
+    t.innerText = "Lucas"   
 }
 
-function h1(){ 
-    let h1 = document.getElementsByTagName("h1")[0]
-    let input = document.getElementById("texto")
 
-    h1.innerText = input.value
-
-    console.log(h1)
-    console.log(input)
-}*/
+*/
